15c287354f6a11663298ef64323040ea8518d873
suyanruo <dimghost1@gmail.com>
2019-01-22 00:01:30 +0800

    *）练习material design

--- app/src/main/java/com/example/zj/androidstudy/activity/TabLayoutActivity.java
+++ app/src/main/java/com/example/zj/androidstudy/activity/TabLayoutActivity.java
@@ -1,16 +1,25 @@
 package com.example.zj.androidstudy.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.zj.androidstudy.R;
 import com.example.zj.androidstudy.adapter.FragmentAdapter;
 import com.example.zj.androidstudy.base.BaseActivity;
+import com.example.zj.androidstudy.fragment.ListFragment;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -20,6 +29,8 @@ public class TabLayoutActivity extends BaseActivity {
     private String[] mTitles = {"关注", "推荐", "精选", "热点", "体育", "视频", "图片", "娱乐", "科技", "财经", "军事", "健康"};
     private ViewPager mViewPager;
     private TabLayout mTabLayout;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,10 +38,30 @@ public class TabLayoutActivity extends BaseActivity {
         setContentView(R.layout.activity_tablayout);
 
         Toolbar toolbar = findViewById(R.id.toolbar_tab);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         mViewPager = findViewById(R.id.viewpager_tab);
         mTabLayout = findViewById(R.id.tabl_tab);
+        mDrawerLayout = findViewById(R.id.dl_main_drawer);
+        mNavigationView = findViewById(R.id.nv_main_navigation);
+        if (mNavigationView != null) {
+            mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    // 改变item选中状态
+//                    item.setChecked(true);
+                    Toast.makeText(getApplicationContext(), item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                    mDrawerLayout.closeDrawers();
+                    return false;
+                }
+            });
+        }
 
-        setSupportActionBar(toolbar);
         initViewPager();
     }
 
@@ -52,5 +83,21 @@ public class TabLayoutActivity extends BaseActivity {
         // 给TagLayout设置适配器
         mTabLayout.setTabsFromPagerAdapter(fragmentAdapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_overaction, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

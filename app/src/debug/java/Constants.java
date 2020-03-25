2fd24057095c6234ba0fdbc1c2a18ef3967b8fe0
jianzhang <jianzhang@yangqianguan.com>
2020-03-25 17:10:32 +0800

    +）自定义task；gradle配置产品类型
    ref: https://developer.android.com/studio/build/build-variants?hl=zh-cn

---
+++ app/src/debug/java/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * Created on 2019-10-23.
+ */
+public class Constants {
+
+  /**
+   * FireBaseCloudMessage设备token
+   */
+  public static final String SP_FIRE_BASE_CLOUD_MESSAGE_TOKEN = "fireBaseCloudMessageToken";
+  /**
+   * FireBaseCloudMessage通用订阅的主题
+   */
+  public static final String FIRE_BASE_CLOUD_MESSAGE_TOPIC = "all";
+}

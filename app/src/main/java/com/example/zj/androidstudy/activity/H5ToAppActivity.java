aa5afb046f8d56ef268c3fbfad8ec818079bbd6a
jianzhang <jianzhang@yangqianguan.com>
2020-11-12 20:35:03 +0800

    *）View滑动应用 ref：Android开发艺术探索

--- app/src/main/java/com/example/zj/androidstudy/activity/H5ToAppActivity.java
+++ app/src/main/java/com/example/zj/androidstudy/activity/H5ToAppActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Button;
 
 import com.example.zj.androidstudy.R;
 
@@ -18,7 +19,8 @@ public class H5ToAppActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_h5_to_app);
 
-    findViewById(R.id.btn_go_main).setOnClickListener(new View.OnClickListener() {
+    final Button btnGoMain = findViewById(R.id.btn_go_main);
+    btnGoMain.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         webview = findViewById(R.id.webviewh5);
@@ -27,6 +29,14 @@ public class H5ToAppActivity extends AppCompatActivity {
         WebSettings wSet = webview.getSettings();
         wSet.setJavaScriptEnabled(true);
         webview.loadUrl(url);
+      }
+    });
+
+    findViewById(R.id.btn_move).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // scrollBy() scrollTo() 只改变控件内容位置，不改变控件位置
+        btnGoMain.scrollBy(-100, 0);
       }
     });
   }

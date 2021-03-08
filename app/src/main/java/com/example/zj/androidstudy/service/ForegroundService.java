1b86c281e37542880b3fbf2aadae7db734c5c47f
jianzhang <jianzhang@yangqianguan.com>
2021-03-08 15:57:35 +0800

    +）service启动dialog

--- app/src/main/java/com/example/zj/androidstudy/service/ForegroundService.java
+++ app/src/main/java/com/example/zj/androidstudy/service/ForegroundService.java
@@ -1,33 +1,50 @@
 package com.example.zj.androidstudy.service;
 
+import android.app.Dialog;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
+
+import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.example.zj.androidstudy.R;
 import com.example.zj.androidstudy.material.MaterialDesignActivity;
 import com.example.zj.androidstudy.tool.NotificationUtil;
 
 public class ForegroundService extends Service {
-    private static final String TAG = "ForegroundService";
-    
-    public ForegroundService() {
-    }
+  private static final String TAG = "ForegroundService";
+  private Handler mHandler;
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Log.e(TAG, "onCreate");
+  public ForegroundService() {
+  }
 
+  @Override
+  public void onCreate() {
+      super.onCreate();
+      Log.e(TAG, "onCreate");
 
-        createAndShowForegroundNotification(1);
-    }
+      createAndShowForegroundNotification(1);
+
+      mHandler = new Handler(Looper.getMainLooper());
+      mHandler.post(new Runnable() {
+          @Override
+          public void run() {
+              createDialog();
+          }
+      });
+  }
 
     private void createAndShowForegroundNotification(int notificationId) {
         Intent intent = new Intent(this, MaterialDesignActivity.class);
@@ -57,7 +74,20 @@ public class ForegroundService extends Service {
 //        lastShownNotificationId = notificationId;
     }
 
-
+    private void createDialog() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Settings.canDrawOverlays(this)) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext(), R.style.AlertDialogCustom);
+            builder.setTitle("Service Dialog");
+            Dialog dialog = builder.create();
+            //8.0系统加强后台管理，禁止在其他应用和窗口弹提醒弹窗，如果要弹，必须使用TYPE_APPLICATION_OVERLAY，否则弹不出
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                dialog.getWindow().setType((WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY));
+            } else {
+                dialog.getWindow().setType((WindowManager.LayoutParams.TYPE_SYSTEM_ALERT));
+            }
+            dialog.show();
+        }
+    }
 
     @Override
     public IBinder onBind(Intent intent) {
--- app/src/main/java/com/example/zj/androidstudy/service/ServiceActivity.java
+++ app/src/main/java/com/example/zj/androidstudy/service/ServiceActivity.java
@@ -4,11 +4,17 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+
 import androidx.annotation.Nullable;
+
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.zj.androidstudy.R;
 import com.example.zj.androidstudy.base.BaseActivity;
@@ -63,6 +69,8 @@ public class ServiceActivity extends BaseActivity {
                 unbindService();
             }
         });
+
+         checkAlertPermission();
     }
 
     private void startService() {
@@ -89,5 +97,29 @@ public class ServiceActivity extends BaseActivity {
     private void unbindService() {
         unbindService(mServiceConnection);
         mBtnUnbindService.setEnabled(false);
+    }
+
+    private void checkAlertPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 1);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "未被授予权限，相关功能不可用", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }

f5cdf6cef2b12b5695a3aa476ffbe1c3ab1584c5
zhangjian <dimghost1@gmail.com>
2022-01-21 18:39:17 +0800

    +）加载长图 ref: https://www.jianshu.com/p/2bf2bee496aa

--- app/src/main/java/com/example/zj/androidstudy/bigImage/LargeImageViewActivity.java
+++ app/src/main/java/com/example/zj/androidstudy/bigImage/LargeImageViewActivity.java
@@ -1,30 +1,35 @@
 package com.example.zj.androidstudy.bigImage;
 
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 import com.example.zj.androidstudy.R;
 
 import java.io.IOException;
 import java.io.InputStream;
 
-public class LargeImageViewActivity extends AppCompatActivity
-{
-    private LargeImageView mLargeImageView;
+public class LargeImageViewActivity extends AppCompatActivity {
+//    private LargeImageView mLargeImageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_large_image_view);
 
-        mLargeImageView = findViewById(R.id.id_largetImageview);
-        try {
-            InputStream inputStream = getAssets().open("qm.jpg");
-            mLargeImageView.setInputStream(inputStream);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        mLargeImageView = findViewById(R.id.id_largetImageview);
+//        try {
+//            InputStream inputStream = getAssets().open("qm.jpg");
+//            mLargeImageView.setInputStream(inputStream);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+        imageView.setMinimumScaleType(SubsamplingScaleImageView.SCALE_TYPE_START);
+        imageView.setOrientation(SubsamplingScaleImageView.ORIENTATION_90);
+        imageView.setImage(ImageSource.asset("qm.jpg"));
     }
 }
